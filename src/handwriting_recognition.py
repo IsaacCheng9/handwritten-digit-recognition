abebf2ae790506d1b9718cf992ed32a854e89d5e
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-14 01:14:02 +0100

    Add decision boundaries to graph plot

--- src/handwriting_recognition.py
+++ src/handwriting_recognition.py
@@ -14,17 +14,47 @@ def show_data_sample_details():
     return data
 
 
+def analyse_handwritten_digits(data):
+    k_means, reduced_data = k_means_clustering(data)
+    create_decision_boundaries(k_means, reduced_data)
+    plot_cluster_graph(k_means, reduced_data)
+
+
 def k_means_clustering(data):
     # Reduces the data to points in 2D space.
     reduced_data = PCA(n_components=2).fit_transform(data)
     # Creates a k-means object with 10 clusters, each representing a digit.
     k_means = KMeans(n_clusters=10)
     k_means.fit(reduced_data)
+    return k_means, reduced_data
 
+
+def create_decision_boundaries(k_means, reduced_data):
+    # Sets visual quality of the boundaries.
+    h = .01
+    # Plots decision boundaries for clusters.
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    mesh_x, mesh_y = np.meshgrid(np.arange(x_min, x_max, h),
+                                 np.arange(y_min, y_max, h))
+    # Obtains labels for each point in mesh.
+    mesh_labels = k_means.predict(np.c_[mesh_x.ravel(), mesh_y.ravel()])
+    # Plots resultant mesh clusters.
+    mesh_labels = mesh_labels.reshape(mesh_x.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(mesh_labels, interpolation="nearest",
+               extent=(mesh_x.min(), mesh_x.max(), mesh_y.min(), mesh_y.max()),
+               cmap=plt.cm.tab10, aspect="auto", origin="lower")
+
+
+def plot_cluster_graph(k_means, reduced_data):
     # Plots the resultant k-means clusters onto a graph.
     centroids = k_means.cluster_centers_
+    # Adds the clusters of the data to the graph in different colours.
     plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=k_means.labels_,
                 cmap="rainbow", s=8)
+    # Marks centroids with red dots.
     plt.scatter(centroids[:, 0], centroids[:, 1], color="red")
     plt.title("K-Means Clustering on the Digits Data Set Using PCA-Reduced "
               "Data\n(centroids marked with red dots)")
@@ -33,7 +63,7 @@ def k_means_clustering(data):
 
 def main():
     data = show_data_sample_details()
-    k_means_clustering(data)
+    analyse_handwritten_digits(data)
 
 
 if __name__ == "__main__":

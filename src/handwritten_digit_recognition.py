5845a672a3f3f47bda5ce9e9ac2f0ca790b60082
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-15 03:29:34 +0100

    Add evaluation of accuracy

--- src/handwritten_digit_recognition.py
+++ src/handwritten_digit_recognition.py
@@ -3,6 +3,8 @@ A program to perform machine learning using k-means clustering over a sample of
 handwritten digits. It creates the clusters, and plots them in a graph for the
 user to see.
 """
+from collections import Counter
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
@@ -39,8 +41,48 @@ def k_means_clustering(data):
     reduced_data = PCA(n_components=2).fit_transform(data)
     # Creates a k-means object with 10 clusters, each representing a digit.
     k_means = KMeans(n_clusters=10)
-    k_means.fit(reduced_data)
-    return k_means, reduced_data
+    cluster_numbers = k_means.fit_predict(reduced_data)
+
+    return k_means, reduced_data, cluster_numbers
+
+
+def evaluate_accuracy(data):
+    """
+    Evaluates accuracy of the k-means clustering analysis.
+
+    Args:
+        data: The sample data used for handwritten digit recognition.
+    """
+    digits = load_digits()
+
+    correct_predictions = 0
+    for _ in range(5):
+        _, _, cluster_numbers = k_means_clustering(data)
+        cluster_groups = determine_cluster_representations(cluster_numbers,
+                                                           digits)
+        for number in range(len(cluster_numbers)):
+            real_value = digits.target[number]
+            cluster_index = cluster_numbers[number]
+            if real_value == cluster_groups[cluster_index]:
+                correct_predictions += 1
+    print("Accuracy (%): {}".format(correct_predictions / 1797 * 20))
+
+
+def determine_cluster_representations(cluster_numbers, digits):
+    # Creates a dictionary to group the real values within each cluster.
+    cluster_groups = {}
+    for number in range(len(cluster_numbers)):
+        real_value = digits.target[number]
+        cluster_index = cluster_numbers[number]
+        try:
+            cluster_groups[cluster_index].append(real_value)
+        except KeyError:
+            cluster_groups[cluster_index] = [real_value]
+    # Identifies each cluster representation based on majority vote.
+    for key in cluster_groups:
+        cluster_representation = Counter(cluster_groups[key]).most_common(1)
+        cluster_groups[key] = cluster_representation[0][0]
+    return cluster_groups
 
 
 def create_decision_boundaries(k_means, reduced_data):
@@ -52,7 +94,7 @@ def create_decision_boundaries(k_means, reduced_data):
         reduced_data: Sample data reduced to 2D space.
     """
     # Sets visual quality of the boundaries (lower is better).
-    quality = .01
+    quality = 0.05
     # Plots decision boundaries for clusters.
     x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
     y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
@@ -95,9 +137,10 @@ def main():
     Performs the handwritten digit recognition using k-means clustering.
     """
     data = show_data_sample_details()
-    k_means, reduced_data = k_means_clustering(data)
+    k_means, reduced_data, cluster_numbers = k_means_clustering(data)
     create_decision_boundaries(k_means, reduced_data)
     plot_cluster_graph(k_means, reduced_data)
+    evaluate_accuracy(data)
 
 
 if __name__ == "__main__":
